3905febaaa12f70c5f00ccd06538bdff7cb37f51
kevinkerin <kevin.kerin@gmail.com>
2020-05-20 22:00:44 +1000

    Initial commit

---
+++ app/src/main/java/com/example/weather_app/Item.kt
@@ -0,0 +1,3 @@
+package com.example.weather_app
+
+data class Item (val imageResource: Int, val text1: String, val text2: String)
---
+++ app/src/main/java/com/example/weather_app/MyAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.weather_app
+
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter : RecyclerView.Adapter() {
+
+    class ViewHolder : RecyclerView.ViewHolder {
+
+    }
+
+}
