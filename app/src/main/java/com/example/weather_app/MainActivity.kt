3f4929560b0210a47d5dc566c5d0d4cbd59613a7
kevinkerin <kevin.kerin@gmail.com>
2020-05-20 23:23:59 +1000

    Added JSON

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.weather_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getJson()
+
     }
+
+    fun getJson(){
+
+        val url = "http://dnu5embx6omws.cloudfront.net/venues/weather.json"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Error")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body?.string()
+                println(body)
+            }
+        })
+
+    }
+
 }
--- app/src/main/java/com/example/weather_app/MyAdapter.kt
+++ app/src/main/java/com/example/weather_app/MyAdapter.kt
@@ -1,11 +1,34 @@
 package com.example.weather_app
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item.view.*
 
-class MyAdapter : RecyclerView.Adapter() {
+class MyAdapter(private val itemList: List<Item>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
 
-    class ViewHolder : RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        return ViewHolder(itemView)
+    }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = itemList[position]
+
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView1.text = currentItem.text1
+        holder.textView2.text = currentItem.text2
+    }
+
+    override fun getItemCount() = itemList.size
+
+    class ViewHolder(itemView: View ) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.image_view
+        val textView1: TextView = itemView.text_view_1
+        val textView2: TextView = itemView.text_view_2
     }
 
 }
