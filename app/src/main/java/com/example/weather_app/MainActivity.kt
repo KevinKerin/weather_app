6ab8af482759b42f5a95532442b2f7a1142756b4
kevinkerin <kevin.kerin@gmail.com>
2020-05-20 23:41:40 +1000

    Added GSON

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.weather_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.GsonBuilder
 import okhttp3.*
 import java.io.IOException
 
@@ -24,16 +25,29 @@ class MainActivity : AppCompatActivity() {
         val client = OkHttpClient()
 
         client.newCall(request).enqueue(object: Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                println("Error")
-            }
-
             override fun onResponse(call: Call, response: Response) {
                 val body = response?.body?.string()
                 println(body)
+
+                val gson = GsonBuilder().create()
+
+                val data = gson.fromJson(body, Data::class.java)
+
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                println("Error")
             }
         })
 
     }
 
 }
+
+class Data(val ret: Boolean, val isOkay: Boolean, val data: List<City>)
+
+class City(val _venueId: String, val _name: String, val _country: Country, val _weatherCondition: String, val  _weatherConditionIcon: String, val _weatherWind: String,
+           val _weatherHumidity: String, val _weatherTemp: String, val _weatherFeelsLike: String, val _sport: Sport, val _weatherLastUpdated: String)
+
+class Country(val _countryID: String, val _name: String)
+
+class Sport(val _sportID: String, val _description: String)
